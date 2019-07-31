45e902bbe42bbaa2e1070a58f923d5f4cc37e955
edelBCS <edelm8@gmail.com>
2019-07-30 19:19:12 -0700

    added html creation function and firebase initialization

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,33 @@
-$("#addEmployeeBtn").on("click", function(){
-    $()
+var name;
+var role;
+var startDate;
+var monthlyRate;
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCTKU-AJ1YJWpzrWYlS5nChgEOePh5UDng",
+    authDomain: "fir-intro-91881.firebaseapp.com",
+    databaseURL: "https://fir-intro-91881.firebaseio.com",
+    projectId: "fir-intro-91881",
+    storageBucket: "",
+    messagingSenderId: "8542128191",
+    appId: "1:8542128191:web:5729308456045ce4"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+var dbRef = database.ref();
+
+
+$("#addEmployeeBtn").on("click", function () {
+    var newEmployee = $("<tr>")
+    newEmployee
+        .append($("<td>").text(name))
+        .append($("<td>").text(role))
+        .append($("<td>").text(startDate))
+        .append($("<td>").text(monthlyRate));
+
+    $("#tableID tbody").append(newEmployee);
 });
